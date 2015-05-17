d450609df5290dfdc3ce5ef03dd828b75ef2d6f7
n0fate <n0fate@n0fate.com>
2015-05-17 14:01:27 +0900

    remove a code related debugging
    remove a code related debugging

--- wtmpx_solaris.py
+++ wtmpx_solaris.py
@@ -53,7 +53,7 @@ contentlist = []
 dumppage = []
 entries, dumppage = who()
 
-f = open("output.bin", "wb")
+# f = open("output.bin", "wb")
 
 for i in range(0, len(entries)):
 	#print int(entries[i]['type'])
@@ -80,12 +80,12 @@ for i in range(0, len(entries)):
 	line.append('')
 	contentlist.append(line)
 
-	if len(entries[i]['ipaddress']) == 0 and entries[i]['user'] != 'cjones' and entries[i]['user'] != 'skyoo':
-		f.write(dumppage[i])
-	if entries[i]['ipaddress'] == '8.80.4.21:0' and entries[i]['user'] != 'cjones' and entries[i]['user'] != 'skyoo':
-		f.write(dumppage[i])
+# 	if len(entries[i]['ipaddress']) == 0 and entries[i]['user'] != 'cjones' and entries[i]['user'] != 'skyoo':
+# 		f.write(dumppage[i])
+# 	if entries[i]['ipaddress'] == '8.80.4.21:0' and entries[i]['user'] != 'cjones' and entries[i]['user'] != 'skyoo':
+# 		f.write(dumppage[i])
 
-f.close()
+# f.close()
 
 mszlist = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
 columnprint(headerlist, contentlist, mszlist)
